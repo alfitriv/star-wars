708cec4e17bb94508477b442053b069d396e45cf
Mila Alfitri <alfitriv@gmail.com>
2019-01-05 20:12:51 +0700

    Initial Commit

---
+++ MayTheForce/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MayTheForce
+//
+//  Created by Vania Radmila Alfitri on 05/01/19.
+//  Copyright © 2019 Vania Radmila Alfitri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
